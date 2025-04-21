2261564a569519aeb320b287865b55198fba6251
Brayden Boyko <braydenboyko@boykowealth.com>
2025-04-21 11:48:35 -0600

    Setup for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fredApi",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["pandas"],
+    author="Brayden Boyko",
+    description="This Python module provides a simple interface to fetch and parse economic data series from the Federal Reserve Economic Data (FRED) API. Using this module, you can retrieve time series data from FRED, including economic indicators, inflation rates, and other key datasets.",
+    url="https://github.com/boykowealth/fred_api",
+)
